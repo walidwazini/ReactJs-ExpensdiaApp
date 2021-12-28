7fc9352f03aeb28ff7f000159ba015bf738cc649
Walid Wazini <wazini25@gmail.com>
2021-12-28 15:21:29 +0800

    binding

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -48,6 +48,9 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         }
         console.log(newExpense)
+        setEnteredDate('')
+        setEnteredTitle('')
+        setEnteredAmount('')
     }
 
     return (
@@ -99,7 +102,11 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input 
+                    type='text'
+                    value={enteredTitle} 
+                    onChange={titleChangeHandler} 
+                    />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
@@ -107,6 +114,7 @@ const ExpenseForm = () => {
                         type='number'
                         min='0.01'
                         step='0.01'
+                        value={enteredAmount}
                         onChange={amountChangeHandler}
                     />
                 </div>
@@ -116,6 +124,7 @@ const ExpenseForm = () => {
                         type='date'
                         min='2019-01-01'
                         max='2022-12-31'
+                        value={enteredDate}
                         onChange={dateChangeHandler}
                     />
                 </div>
